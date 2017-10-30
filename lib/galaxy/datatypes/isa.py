28520da844568e67259b7495cf9991c2ff1d609f
Pierrick Roger <pk.roger@icloud.com>
2017-10-30 11:22:35 +0100

    Clean and add comments.

--- lib/galaxy/datatypes/isa.py
+++ lib/galaxy/datatypes/isa.py
@@ -18,7 +18,8 @@ import logging
 import tarfile
 import tempfile
 import csv
-from isatools import isatab #==> XXX ImportError: cannot import name zip_longest. Is isatools compatible with Python 2.7?
+# TODO Import of isatools-lite library for reading ISA-Tab and ISA-Json archives.
+#from isatools import isatab ==> XXX ImportError: cannot import name zip_longest. Is isatools compatible with Python 2.7?
 #from isatools import isajson
 from json import dumps
 from io import BytesIO
@@ -28,7 +29,7 @@ from galaxy.datatypes import data
 from galaxy.datatypes import metadata
 from galaxy.util.sanitize_html import sanitize_html
 
-# the name of file containing the isa archive
+# The name of the ISA archive (compressed file) as saved inside Galaxy
 ISA_ARCHIVE_NAME = "archive"
 
 # Archives types
@@ -53,30 +54,44 @@ logger.propagate = False
 logger.addHandler(ch)
 logger.setLevel(logging.DEBUG)
 
+# ISA class {{{1
+################################################################
+
 class Isa(data.Data):
-    
+
+    """ Base class for implementing ISA datatypes """
+    file_ext = "isa"
+    composite_type = 'auto_primary_file'
+    allow_datatype_change = False
+    is_binary = True
+
+    # Included investigation classes {{{2
+    ################################################################
+    # XXX They are used to repace missing isatools library.
+    # Once isatools is integrated, these classes will go away.
+
     class Investigation(object):
         filename = None
         identifier = ''
         title = ''
         studies = []
-        
+
         def __init__(self, filename):
             self.filename = filename
             self.parse()
-        
+
     class Study(object):
         identifier = ''
         title = ''
         description = ''
         submission_date = ''
         public_release_date = ''
-        
+
     class InvestigationTab(Investigation):
-        
+
         def __init__(self, filename):
 	    super(self.__class__, self).__init__(filename)
-        
+
         def parse(self):
             self.identifier = ''
             self.title = ''
@@ -108,12 +123,12 @@ class Isa(data.Data):
                             study.public_release_date = row[1]
                 if study is not None:
                     self.studies.append(study)
-        
+
     class InvestigationJson(Investigation):
-        
+
         def __init__(self, filename):
 	    super(self.__class__, self).__init__(filename)
-        
+
         def parse(self):
             self.identifier = ''
             self.title = ''
@@ -141,55 +156,57 @@ class Isa(data.Data):
         if filename[-5:].lower() == '.json':
             return Isa.InvestigationJson(filename)
         return Isa.InvestigationTab(filename)
-        
-    """ Base class for implementing ISA datatypes """
-    file_ext = "isa"
-    composite_type = 'auto_primary_file'
-    allow_datatype_change = False
-    is_binary = True
 
-    # metadata.MetadataElement(name="base_name", desc="base name isa tab dataset",
-    #                          default='Text',
-    #                          readonly=True, set_in_upload=True)
+    # Constructor {{{2
+    ################################################################
 
     def __init__(self, **kwd):
         data.Data.__init__(self, **kwd)
+
+        # Add the archive file as the only composite file
         self.add_composite_file(ISA_ARCHIVE_NAME, is_binary=True, optional=True)
-        
+
+    # Get main file {{{2
+    ################################################################
+
     @classmethod
     def _get_main_file(cls, dataset):
-        
+        """Get the main file of the ISA type: either the ISA-Tab investigation file, or the ISA-Json JSON file."""
+
         main_file = None
-        
+
         # Detect type
         if dataset and dataset.dataset and dataset.dataset.extra_files_path and os.path.exists(dataset.dataset.extra_files_path):
-            
+
             # Get ISA archive older
             isa_folder = dataset.dataset.extra_files_path
             isa_files = os.listdir(isa_folder)
-            
+
             # Try to find an ISA-Tab investigation file
             main_file = cls._find_isatab_investigation_filename(isa_files)
-            
+
             # Try to find an ISA-Json JSON file
             if main_file is None:
                 main_file = cls._find_isajson_json_filename(isa_files)
-                
+
             # Make full path
             if main_file is not None:
                 main_file = os.path.join(isa_folder, main_file)
-                
+
             if main_file is None:
                 logger.warning('Unknown ISA archive type. Cannot determine if it is ISA-Tab or ISA-Json. Cannot find a main file.')
         else:
             logger.warning('Unvalid dataset object, or no extra files path found for this dataset.')
-            
+
         return main_file
-    
+
+    # Get investigation {{{2
+    ################################################################
+
     @classmethod
     def _get_investigation(cls, dataset):
-        """Create a contained instance specific to the exact ISA type (Tab or Json)."""
-        
+        """Create a contained instance specific to the exact ISA type (Tab or Json). We will use it to parse and access information from the archive."""
+
         investigation = None
         main_file = cls._get_main_file(dataset)
         if main_file is not None:
@@ -197,9 +214,12 @@ class Isa(data.Data):
 
         return investigation
 
+    # Find ISA-Tab investigation filename {{{2
+    ################################################################
+
     @classmethod
     def _find_isatab_investigation_filename(cls, files_list):
-        """Find the investigation file of an ISA-Tab."""
+        """Find the investigation file inside the ISA-Tab archive."""
         logger.debug("Finding investigation filename assuming an ISA-Tab dataset...")
         res = []
         for f in files_list:
@@ -217,9 +237,12 @@ class Isa(data.Data):
             logger.error("More than one file match the pattern 'i_*.txt' to identify the investigation file")
         return None
 
+    # Find ISA-Json JSON filename {{{2
+    ################################################################
+
     @classmethod
     def _find_isajson_json_filename(cls, files_list):
-        """Find the JSON file of an ISA-Json."""
+        """Find the JSON file inside the ISA-Json archive."""
         logger.debug("Finding investigation filename assuming an ISA-JSON dataset...")
         res = [f for f in files_list if f.endswith(".json")]
         logger.debug("List of matches: %r", res)
@@ -231,7 +254,12 @@ class Isa(data.Data):
             logger.error("More than one JSON file match the pattern to identify the investigation file")
         return None
 
+    # Extract archive {{{2
+    ################################################################
+
     def _extract_archive(self, stream, output_path=None):
+        """Extract files from archive and put them is predefined folder."""
+
         # extract the archive to a temp folder
         if output_path is None:
             output_path = tempfile.mkdtemp()
@@ -247,7 +275,64 @@ class Isa(data.Data):
 
         return output_path
 
+    # Extract ZIP archive {{{2
+    ################################################################
+
+    def _extract_zip_archive(self, stream, target_path):
+        """Extract files from a ZIP archive."""
+
+        logger.debug("Isa::_extract_zip_archive")
+        logger.debug("Decompressing the ZIP archive")
+        temp_folder = tempfile.mkdtemp()
+        data = BytesIO(stream.read())
+        zip_ref = zipfile.ZipFile(data)
+        zip_ref.extractall(path=temp_folder)
+        self._move_to_target_path(temp_folder, target_path)
+
+    # Extract TAR archive {{{2
+    ################################################################
+
+    def _extract_tar_archive(self, stream, target_path):
+        """Extract files from a TAR archive."""
+
+        logger.debug("Isa::_extract_tar_archive")
+        # extract the TAR archive
+        logger.debug("Decompressing the TAR archive")
+        temp_folder = tempfile.mkdtemp()
+        with tarfile.open(fileobj=stream) as tar:
+            tar.extractall(path=temp_folder)
+        self._move_to_target_path(temp_folder, target_path)
+
+    # Move to target path {{{2
+    ################################################################
+
+    def _move_to_target_path(self, temp_folder, target_path, delete_temp_folder=True):
+        """Move extracted files to the destination folder imposed by Galaxy."""
+
+        logger.debug("Isa::_move_to_target_path")
+        # find the root folder containing the dataset
+        tmp_subfolders = [f for f in os.listdir(temp_folder) if
+                          not f.startswith(".") and f not in (ISA_ARCHIVE_NAME, "__MACOSX")]
+        logger.debug("Files within the temp folder: %r", tmp_subfolders)
+        # move files contained within the root dataset folder to their target path
+        root_folder = os.path.join(temp_folder, tmp_subfolders[0])
+        if len(tmp_subfolders) == 1 and os.path.isdir(root_folder):
+            # move the root dataset folder to its final destination and clean the temp data
+            for f in os.listdir(root_folder):
+                shutil.move(os.path.join(root_folder, f), target_path)
+        elif len(tmp_subfolders) > 1:
+            for f in tmp_subfolders:
+                shutil.move(os.path.join(temp_folder, f), target_path)
+        # clean temp data if required
+        if delete_temp_folder:
+            shutil.rmtree(temp_folder)
+
+    # List archive files {{{2
+    ################################################################
+
     def _list_archive_files(self, stream):
+        """List files contained inside the ISA archive."""
+
         logger.debug("Isa::_list_archive_files")
         # try to detect the type of the compressed archive
         a_type = self._detect_file_type(stream)
@@ -277,12 +362,14 @@ class Isa(data.Data):
                 files_list = [f.replace(base_path, '') for f in files_list]
         return files_list
 
+    # Detect file type {{{2
+    ################################################################
+
     def _detect_file_type(self, stream):
         """
-        Try to detect the type of the dataset archive.
+        Try to detect the type of the ISA archive: is it ZIP, or GUNZIP?
 
-        :param dataset:
-        :return: _ZIP or _TAR if the file type is detected; None otherwise.
+        :return: "zip" or "gz" if the file type is detected; None otherwise.
         """
         logger.debug("Isa::_detect_file_type")
         file_type = None
@@ -292,16 +379,20 @@ class Isa(data.Data):
         if matched_prefix:
             file_type = _FILE_TYPE_PREFIX[matched_prefix.string[matched_prefix.start():matched_prefix.end()]]
         logger.debug("Detected file type: %s (prefix: %r)" % (file_type, file_start))
+
         return file_type
 
+    # Set peek {{{2
+    ################################################################
+
     def set_peek(self, dataset, is_multi_byte=False):
-        """Set the peek and blurb text"""
-        
+        """Set the peek and blurb text. Get first lines of the main file and set it as the peek."""
+
         main_file = self._get_main_file(dataset)
-        
+
         if main_file is None:
             raise RuntimeError("Unable to find the main file within the 'files_path' folder")
-        
+
         # Read first lines of main file
         with open(main_file, "r") as f:
             data = []
@@ -317,8 +408,12 @@ class Isa(data.Data):
                 dataset.peek = 'file does not exist'
                 dataset.blurb = 'file purged from disk'
 
+    # Display peek {{{2
+    ################################################################
+
     def display_peek(self, dataset):
-        """Create HTML table, used for displaying peek"""
+        """Create the HTML table used for displaying peek, from the peek text found by set_peek() method."""
+
         out = ['<table cellspacing="0" cellpadding="3">']
         try:
             if not dataset.peek:
@@ -335,44 +430,12 @@ class Isa(data.Data):
             out = "Can't create peek %s" % str(exc)
         return out
 
-    def _extract_zip_archive(self, stream, target_path):
-        logger.debug("Isa::_extract_zip_archive")
-        logger.debug("Decompressing the ZIP archive")
-        temp_folder = tempfile.mkdtemp()
-        data = BytesIO(stream.read())
-        zip_ref = zipfile.ZipFile(data)
-        zip_ref.extractall(path=temp_folder)
-        self._move_to_target_path(temp_folder, target_path)
-
-    def _extract_tar_archive(self, stream, target_path):
-        logger.debug("Isa::_extract_tar_archive")
-        # extract the TAR archive
-        logger.debug("Decompressing the TAR archive")
-        temp_folder = tempfile.mkdtemp()
-        with tarfile.open(fileobj=stream) as tar:
-            tar.extractall(path=temp_folder)
-        self._move_to_target_path(temp_folder, target_path)
-
-    def _move_to_target_path(self, temp_folder, target_path, delete_temp_folder=True):
-        logger.debug("Isa::_move_to_target_path")
-        # find the root folder containing the dataset
-        tmp_subfolders = [f for f in os.listdir(temp_folder) if
-                          not f.startswith(".") and f not in (ISA_ARCHIVE_NAME, "__MACOSX")]
-        logger.debug("Files within the temp folder: %r", tmp_subfolders)
-        # move files contained within the root dataset folder to their target path
-        root_folder = os.path.join(temp_folder, tmp_subfolders[0])
-        if len(tmp_subfolders) == 1 and os.path.isdir(root_folder):
-            # move the root dataset folder to its final destination and clean the temp data
-            for f in os.listdir(root_folder):
-                shutil.move(os.path.join(root_folder, f), target_path)
-        elif len(tmp_subfolders) > 1:
-            for f in tmp_subfolders:
-                shutil.move(os.path.join(temp_folder, f), target_path)
-        # clean temp data if required
-        if delete_temp_folder:
-            shutil.rmtree(temp_folder)
+    # Generate primary file {{{2
+    ################################################################
 
     def generate_primary_file(self, dataset=None):
+        """Generate the primary file. It is an HTML file containing description of the composite dataset as well as a list of the composite files that it contains.""".
+
         logger.debug("Isa::generate_primary_file")
         if dataset:
             logger.debug("Dataset: %r", dataset)
@@ -394,11 +457,20 @@ class Isa(data.Data):
             return "\n".join(rval)
         return "<div>No dataset available</div>"
 
+    # Dataset content needs grooming {{{2
+    ################################################################
+
     def dataset_content_needs_grooming(self, file_name):
         """This function is called on an output dataset file after the content is initially generated."""
         return True
 
+    # Groom dataset content {{{2
+    ################################################################
+
     def groom_dataset_content(self, file_name):
+        """This method is called by Galaxy to extract files contained in a composite data type."""
+        # XXX Is the right place to extract files? Should this step not be a cleaning step instead? Could extracting be done earlier and composite files declared as files contained inside the archive instead of the archive itself?
+
         # extract basename and folder of the current file whose content has to be groomed
         basename = os.path.basename(file_name)
         output_path = os.path.dirname(file_name)
@@ -419,10 +491,15 @@ class Isa(data.Data):
             for f in os.listdir(output_path):
                 logger.debug("File: %s", f)
 
+    # Sniff {{{2
+    ################################################################
+
     def sniff(self, filename):
         """Try to detect whether the actual archive contains an ISA archive simply searching for the existence of an investigation file."""
+        # XXX Remove this method? This method seems to be unused for a composite data type. Inside the uploader tool, only the types of normal datasets can be detected automatically, the types of composite datasets are always specified manually.
+
         logger.debug("Checking if %s is an ISA.", filename)
-        
+
         # Get the list of files within the compressed archive
         with open(filename, 'rb') as stream:
             files_list = self._list_archive_files(stream)
@@ -431,13 +508,21 @@ class Isa(data.Data):
 
         return False
 
+    # Set meta {{{2
+    ################################################################
+
     def set_meta(self, dataset, **kwd):
+        """???What is the purpose of this method?"""
         super(Isa, self).set_meta(dataset, **kwd)
-        
+
+    # Display data {{{2
+    ################################################################
+
     def display_data(self, trans, dataset, preview=False, filename=None, to_ext=None, offset=None, ck_size=None, **kwd):
-        
+        """Returns the HTML page needed for displaying when user click on the eye icon of the composite dataset."""
+
         investigation = self._get_investigation(dataset)
-        
+
         logger.debug('Isa::display_data Investigation %r', investigation)
         if investigation is None:
             html = '<html><header><title>Error while reading ISA archive.</title></header><body><h1>An error occured while reading content of ISA archive.</h1></body></html>'
@@ -453,9 +538,9 @@ class Isa(data.Data):
                 html += '<p>Submitted the %s</p>' % study.submission_date
                 html += '<p>Released on %s</p>' % study.public_release_date
             html += '</body></html>'
-                
+
         # Set mime type
         mime = 'text/html'
         self._clean_and_set_mime_type(trans, mime)
-        
+
         return sanitize_html(html).encode('utf-8')
